f5f9fd97b35487046ebd07413945c39a74c8365b
Novel Bafagih <novel.bafagih@udana.id>
2022-04-18 12:49:11 +0700

    [ADD] entieties export

---
+++ src/entities/index.js
@@ -0,0 +1,11 @@
+const acara = require('./models/acara');
+const artikel = require('./models/artikel');
+const anggota = require('./models/anggota');
+const pendaftar = require('./models/pendaftar');
+
+module.exports = {
+	acara,
+	artikel,
+	anggota,
+	pendaftar	
+}
