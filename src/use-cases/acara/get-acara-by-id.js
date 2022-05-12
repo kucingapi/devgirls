0de01482f7a6034a642db9137e9191e01350c08c
Novel Bafagih <novel.bafagih@udana.id>
2022-05-12 09:16:40 +0700

    [FIX] acara by id when user log in

--- src/use-cases/acara/get-acara-by-id.js
+++ src/use-cases/acara/get-acara-by-id.js
@@ -1,18 +1,28 @@
 const { findAcaraById } = require('../../data-access/acara.db');
+const { findAnggotaById } = require('../../data-access/anggota.db');
 const { sequelizeErrorHandler, UseCaseError } = require('../../entities/error');
+const getPayloadJwt = require('../../functions/getPayloadJwt');
 const { getAcaraByIdValidation } = require('../../validation/acara.validation');
 const validate = require('../../validation/validate');
 
 const makeGetAcaraById = () => {
-  return async function getAcaraById({ params }) {
+  return async function getAcaraById({ params, headers }) {
     validate(getAcaraByIdValidation, params);
     const { id } = params;
-
-    const artikel = await findAcaraById(id).catch(sequelizeErrorHandler);
-    if (artikel === null) {
-      throw new UseCaseError(404, 'aritikel not found');
+    let user = -1;
+    if (headers.authToken) {
+      const payload = getPayloadJwt(headers);
+      user = payload.id;
     }
-    return artikel;
+
+    const acara = await findAcaraById(id).catch(sequelizeErrorHandler);
+    const registered = await acara.hasAnggota(user);
+
+    if (acara === null) {
+      throw new UseCaseError(404, 'acara not found');
+    }
+
+    return {...acara.dataValues, registered};
   };
 };
 
