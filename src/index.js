be9cf040e691665d19f1991c97d52b559c51a0a1
Novel Bafagih <novel.bafagih@udana.id>
2022-05-15 22:50:57 +0700

    [ADD] for heroku

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const { sequelize } = require('./entities/models');
 const app = require('./app');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const nodeEnv = process.env.NODE_ENV.trim();
 
 // Data Base connection
