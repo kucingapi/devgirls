2d40ffbdb248c7d1b14e217d8d82938f2a16b9eb
Novel Bafagih <novel.bafagih@udana.id>
2022-05-15 23:02:59 +0700

    [FIX] dotenv

--- src/database.js
+++ src/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
-const dotenv = require('dotenv');
+const dotenv = require('dotenv') || {};
 
-const env = dotenv.config().parsed;
+const env = dotenv.config().parsed || process.env;
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = env || process.env;
 
 const sequelize = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
