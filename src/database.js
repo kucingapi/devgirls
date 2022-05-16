9e9d0c52551a92f5b78306ea8a591561a141047f
Novel Bafagih <novel.bafagih@udana.id>
2022-05-16 09:24:52 +0700

    [FIX] database connection

--- src/database.js
+++ src/database.js
@@ -2,12 +2,18 @@ const Sequelize = require('sequelize');
 
 const dotenv = require('dotenv') || {};
 
-const env = dotenv.config().parsed; 
+const env = dotenv.config().parsed;
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = env || process.env;
 
 const sequelize = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false // This line will fix new error
+    }
+  },
   define: {
     freezeTableName: true,
   },
