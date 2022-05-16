c2dfb9011edc1eefa0297921d4be3c7df1fadcc2
Novel Bafagih <novel.bafagih@udana.id>
2022-05-16 12:12:25 +0700

    [ADD] sync production

--- src/routes/anggota.routes.js
+++ src/routes/anggota.routes.js
@@ -3,7 +3,14 @@ const makeExpressCallback = require('./make-callback');
 const anggotaController = require('../controllers/anggota.controller');
 
 const router = express.Router();
-router.route('/register').post(makeExpressCallback(anggotaController.registerAnggota));
-router.route('/login').post(makeExpressCallback(anggotaController.loginAnggota));
+router.route('/').get((req, res) => {
+  res.send("hello world")
+});
+router
+  .route('/register')
+  .post(makeExpressCallback(anggotaController.registerAnggota));
+router
+  .route('/login')
+  .post(makeExpressCallback(anggotaController.loginAnggota));
 
 module.exports = router;
