19ed362752ccdf6afe9b01e18de9f42c62e601b0
noveel bafagih <novel.bafagih@udana.id>
2022-05-18 22:23:58 +0700

    [FIX] database developer

--- src/database.js
+++ src/database.js
@@ -5,19 +5,29 @@ const dotenv = require('dotenv') || {};
 const env = dotenv.config().parsed;
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = env || process.env;
 
+const nodeEnv = process.env.NODE_ENV.trim();
+
+let dialectOptions = {};
+let logging = true;
+
+if (nodeEnv === 'production') {
+  dialectOptions = {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false, // This line will fix new error
+    },
+  };
+  logging = false;
+}
+
 const sequelize = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true, // This will help you. But you will see nwe error
-      rejectUnauthorized: false // This line will fix new error
-    }
-  },
+  dialectOptions,
   define: {
     freezeTableName: true,
   },
-  // logging: false,
+  logging
 });
 
 module.exports = { sequelize, Sequelize };
